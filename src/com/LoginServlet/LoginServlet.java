0656c9e625d393dc0f1c0d20e6c537c9fd289d99
Manali Kale <manalikale@Manalis-MacBook-Air.local>
2014-04-12 19:14:10 -0700

    Auth Token

--- src/com/LoginServlet/LoginServlet.java
+++ src/com/LoginServlet/LoginServlet.java
@@ -1,6 +1,8 @@
 package com.LoginServlet;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
@@ -9,11 +11,13 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpUriRequest;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
 
 //import java.io.PrintWriter;
 /**
@@ -32,6 +36,7 @@ public class LoginServlet extends HttpServlet {
     }
 
 	/**
+	 * @return 
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -49,7 +54,12 @@ public class LoginServlet extends HttpServlet {
 			HttpClient client = new DefaultHttpClient();
 			HttpGet req=new HttpGet("https://graph.facebook.com/oauth/access_token?client_id=598128043614342&redirect_uri=http://localhost:8080/FoodBook/LoginServlet&client_secret=3d726e3d1910f42eaa12b8b3dbda4d54&code="+request.getParameter("code"));
 			HttpResponse resp = client.execute(req);
-
+			BufferedReader rd = new BufferedReader(new InputStreamReader(resp.getEntity().getContent()));
+			String str = "";
+			while ((str = rd.readLine()) != null)
+			{
+				out.write("\n"+ str);
+			}
 		}
 	}
 
